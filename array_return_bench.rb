b48e06f6a79913d7f7702ca059ad78a60c0a7127
Matt Dellandrea <pa.mpdellandrea@gmail.com>
2016-02-23 09:36:51 -0600

    spacing correction

--- array_return_bench.rb
+++ array_return_bench.rb
@@ -5,35 +5,35 @@ require 'benchmark/ips'
 a = Array.new(50_000) { |i| i+1 }
 
 Benchmark.ips do |x|
-	x.report 'Using Index' do
-		50_000.times do
-			a[-1]
-		end
-	end
+  x.report 'Using Index' do
+    50_000.times do
+      a[-1]
+    end
+  end
 
-	x.report 'Using Last' do
-		50_000.times do
-			a.last
-		end
-	end
+  x.report 'Using Last' do
+    50_000.times do
+      a.last
+    end
+  end
 	
-	x.report 'Using Slice' do
-		50_000.times do
-			a.slice -1
-		end
-	end
+  x.report 'Using Slice' do
+    50_000.times do
+      a.slice -1
+    end
+  end
 
-	x.report 'Using Fetch' do
-		50_000.times do
-			a.fetch -1
-		end
-	end
+  x.report 'Using Fetch' do
+    50_000.times do
+      a.fetch -1
+    end
+  end
 
-	x.report 'Using At' do
-		50_000.times do
-			a.at -1
-		end
-	end
+  x.report 'Using At' do
+    50_000.times do
+      a.at -1
+    end
+  end
 
-	x.compare!
+  x.compare!
 end
